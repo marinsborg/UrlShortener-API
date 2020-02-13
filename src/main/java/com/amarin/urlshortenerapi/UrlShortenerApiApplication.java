0e8c7ff2342c5559c5d22251c663c7fd8aaeb7f9
antisa <ante.m.marin@gmail.com>
2020-02-14 00:00:14 +0100

    Add project structure

---
+++ src/main/java/com/amarin/urlshortenerapi/UrlShortenerApiApplication.java
@@ -0,0 +1,13 @@
+package com.amarin.urlshortenerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amarin/urlshortenerapi/UrlShortenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amarin.urlshortenerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
